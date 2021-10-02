8e5f93a0bfc5b61e1aea0bf492794627f9f16861
Keith Collister <hnefatl@gmail.com>
2021-10-02 17:05:38 +0200

    Autoformat

--- geolocation.py
+++ geolocation.py
@@ -4,34 +4,33 @@ from typing import List
 
 # Time to wait before turning off lights
 AWAY_DURATION = 30 * 60
-ENTITY = 'person.keith'
+ENTITY = "person.keith"
+
 
 class Geolocation(hass.Hass):
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    self._timer_handler = None
-
-  def initialize(self):
-    self.log('Starting geolocation service')
-    self.listen_state(
-      entity=ENTITY,
-      callback=self.on_zone_change)
-
-  def on_zone_change(self, entity, attribute, old, new, kwargs):
-    self.log(f'{entity} changed zone: was {old}, now {new} ({kwargs})')
-    if new == 'not_home' and self._timer_handler is None:
-      self.log('Starting away timer')
-      self._timer_handler = self.run_in(delay=AWAY_DURATION, callback=self.double_check_away)
-    elif new == 'home':
-      if self._timer_handler is not None:
-        self.log('Cancelling away timer')
-        self.cancel_timer(self._timer_handler)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self._timer_handler = None
-      # TODO: restore lights to normal, should probably write a dedicated light
-      # service for maintaining state and preventing stomping etc.
 
-  def double_check_away(self, kwargs):
-    current_state = self.get_state(entity_id=ENTITY)
-    self.log(f'{ENTITY} current state: {current_state}')
-    if current_state != 'home':
-      self.call_service('light/turn_off', entity_id='group.all_lights')
+    def initialize(self):
+        self.log("Starting geolocation service")
+        self.listen_state(entity=ENTITY, callback=self.on_zone_change)
+
+    def on_zone_change(self, entity, attribute, old, new, kwargs):
+        self.log(f"{entity} changed zone: was {old}, now {new} ({kwargs})")
+        if new == "not_home" and self._timer_handler is None:
+            self.log("Starting away timer")
+            self._timer_handler = self.run_in(delay=AWAY_DURATION, callback=self.double_check_away)
+        elif new == "home":
+            if self._timer_handler is not None:
+                self.log("Cancelling away timer")
+                self.cancel_timer(self._timer_handler)
+                self._timer_handler = None
+            # TODO: restore lights to normal, should probably write a dedicated light
+            # service for maintaining state and preventing stomping etc.
+
+    def double_check_away(self, kwargs):
+        current_state = self.get_state(entity_id=ENTITY)
+        self.log(f"{ENTITY} current state: {current_state}")
+        if current_state != "home":
+            self.call_service("light/turn_off", entity_id="group.all_lights")
--- pager.py
+++ pager.py
@@ -9,99 +9,102 @@ import appdaemon.plugins.hass.hassapi as hass
 
 LIGHT_FLASH_COUNT = 3
 
-IMAP_SERVER = 'imap.gmail.com'
+IMAP_SERVER = "imap.gmail.com"
 IMAP_PORT = 993
-IMAP_IDLE_TIMEOUT = 10 * 60 # 10mins
+IMAP_IDLE_TIMEOUT = 10 * 60  # 10mins
 
-PROCESSED_KEYWORD = 'pager_processed'
+PROCESSED_KEYWORD = "pager_processed"
+
 
 class Pager(hass.Hass):
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    self._connection = None
-    self._main_loop_future = None
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._connection = None
+        self._main_loop_future = None
 
-    self._username = self.args.get('username')
-    self._password = self.args.get('password')
-    self._to_email = self.args.get('email_to')
-    self._from_emails = self.args.get('emails_from')
+        self._username = self.args.get("username")
+        self._password = self.args.get("password")
+        self._to_email = self.args.get("email_to")
+        self._from_emails = self.args.get("emails_from")
 
-    # Create a string like 'OR (FROM a) (OR (FROM b) (FROM c))' (parentheses
-    # for emphasis only) to filter against multiple sender emails.
-    if self._from_emails:
-      joined_from_emails = functools.reduce(lambda l, r: f'OR {l} {r}', map(lambda s: f'FROM {s}', self._from_emails))
-    else:
-      joined_from_emails = ''
-    self._search_string = f'TO {self._to_email} {joined_from_emails} NOT KEYWORD {PROCESSED_KEYWORD}'
+        # Create a string like 'OR (FROM a) (OR (FROM b) (FROM c))' (parentheses
+        # for emphasis only) to filter against multiple sender emails.
+        if self._from_emails:
+            joined_from_emails = functools.reduce(
+                lambda l, r: f"OR {l} {r}", map(lambda s: f"FROM {s}", self._from_emails)
+            )
+        else:
+            joined_from_emails = ""
+        self._search_string = f"TO {self._to_email} {joined_from_emails} NOT KEYWORD {PROCESSED_KEYWORD}"
 
-  def initialize(self):
-    self.log('Starting Pager service')
-    self.log(f'Using query string "{self._search_string}"')
-    if not self._connect():
-      self.log('Pager service failed to initialise')
-    else:
-      self.log('Pager service initialised')
-      self._main_loop_future = self.submit_to_executor(self._main_loop)
+    def initialize(self):
+        self.log("Starting Pager service")
+        self.log(f'Using query string "{self._search_string}"')
+        if not self._connect():
+            self.log("Pager service failed to initialise")
+        else:
+            self.log("Pager service initialised")
+            self._main_loop_future = self.submit_to_executor(self._main_loop)
 
-  def terminate(self):
-    self.log('Terminating Pager service')
-    self._disconnect()
-    self.log('Pager service terminated')
-  
-  def _connect(self):
-    try:
-      self._connection = imapclient.IMAPClient(IMAP_SERVER, IMAP_PORT)
-      self._connection.login(self._username, self._password)
-      self._connection.select_folder(folder='INBOX')
-    except imapclient.exceptions.IMAPClientError as e:
-      self.error(f'Failed to login: {e}')
-      return False
-    return True
-
-  def _disconnect(self):
-    try:
-      self._connection.logout()
-    except Exception as e:
-      # Drop all errors. Maybe not neat, but ensures we keep running.
-      self.log(f'Ignoring disconnect error during terminate: {e}')
-
-  def _main_loop(self):
-    while True:
-      self._connection.idle()
-      idle_responses = self._connection.idle_check(timeout=IMAP_IDLE_TIMEOUT)
-      if idle_responses:
-        self.log(f'IMAP notification: {idle_responses}')
-      self._connection.idle_done()
-      if not idle_responses:
-        # No responses, we must have timed out: refresh the connection to
-        # make sure we don't run into OS/network issues.
-        self.log('Disconnecting and reconnecting for connection health.')
+    def terminate(self):
+        self.log("Terminating Pager service")
         self._disconnect()
-        self._connect()
-        continue
+        self.log("Pager service terminated")
 
-      # Otherwise, we know there's been email activity.
-      uids = self._connection.search(self._search_string)
-      if uids:
-        # Tag the emails so we don't process them again.
-        self.log(f'Adding flag "{PROCESSED_KEYWORD}" to new emails uids {uids}')
-        response = self._connection.add_flags(uids, [PROCESSED_KEYWORD])
-        self.log(f'Keyword response: {response}')
+    def _connect(self):
+        try:
+            self._connection = imapclient.IMAPClient(IMAP_SERVER, IMAP_PORT)
+            self._connection.login(self._username, self._password)
+            self._connection.select_folder(folder="INBOX")
+        except imapclient.exceptions.IMAPClientError as e:
+            self.error(f"Failed to login: {e}")
+            return False
+        return True
 
-        # Flash lights
-        self._red_alert()
+    def _disconnect(self):
+        try:
+            self._connection.logout()
+        except Exception as e:
+            # Drop all errors. Maybe not neat, but ensures we keep running.
+            self.log(f"Ignoring disconnect error during terminate: {e}")
 
-  # Renamed in honour of Maximus
-  def _red_alert(self):
-    bedroom_light_on = self.get_state('group.bedroom_lights') == 'on'
+    def _main_loop(self):
+        while True:
+            self._connection.idle()
+            idle_responses = self._connection.idle_check(timeout=IMAP_IDLE_TIMEOUT)
+            if idle_responses:
+                self.log(f"IMAP notification: {idle_responses}")
+            self._connection.idle_done()
+            if not idle_responses:
+                # No responses, we must have timed out: refresh the connection to
+                # make sure we don't run into OS/network issues.
+                self.log("Disconnecting and reconnecting for connection health.")
+                self._disconnect()
+                self._connect()
+                continue
 
-    for i in range(LIGHT_FLASH_COUNT):
-      self.call_service('scene/turn_on', entity_id='scene.bedroom_red')
-      self.call_service('scene/turn_on', entity_id='scene.office_red')
-      self.sleep(1)
-      self.call_service('scene/turn_on', entity_id='scene.bedroom_dim')
-      self.call_service('scene/turn_on', entity_id='scene.office_concentrate')
-      self.sleep(1)
+            # Otherwise, we know there's been email activity.
+            uids = self._connection.search(self._search_string)
+            if uids:
+                # Tag the emails so we don't process them again.
+                self.log(f'Adding flag "{PROCESSED_KEYWORD}" to new emails uids {uids}')
+                response = self._connection.add_flags(uids, [PROCESSED_KEYWORD])
+                self.log(f"Keyword response: {response}")
 
-    if not bedroom_light_on:
-      self.call_service('light/turn_off', entity_id='group.bedroom_lights')
+                # Flash lights
+                self._red_alert()
+
+    # Renamed in honour of Maximus
+    def _red_alert(self):
+        bedroom_light_on = self.get_state("group.bedroom_lights") == "on"
+
+        for i in range(LIGHT_FLASH_COUNT):
+            self.call_service("scene/turn_on", entity_id="scene.bedroom_red")
+            self.call_service("scene/turn_on", entity_id="scene.office_red")
+            self.sleep(1)
+            self.call_service("scene/turn_on", entity_id="scene.bedroom_dim")
+            self.call_service("scene/turn_on", entity_id="scene.office_concentrate")
+            self.sleep(1)
+
+        if not bedroom_light_on:
+            self.call_service("light/turn_off", entity_id="group.bedroom_lights")
