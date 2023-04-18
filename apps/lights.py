24ccde6b4b813da5adc5c61cd0ec655b8ec9aec3
Keith <hnefatl@gmail.com>
2023-04-19 00:09:22 +0200

    Finally fix bug in lighting where motion while a light is turning off fails to turn the lights back on.

--- apps/lights.py
+++ apps/lights.py
@@ -12,9 +12,7 @@ import appdaemon.plugins.hass.hassapi as hass  # type: ignore
 _VERBOSE_LOG = True
 
 
-def callback(
-    f: Callable[[Room, str, str, str, str, Any], None]
-) -> Callable[[Room, str, str, str, str, Any], None]:
+def callback(f: Callable[[Room, str, str, str, str, Any], None]) -> Callable[[Room, str, str, str, str, Any], None]:
     """Provides type checking and early bail-out if manual control is enabled"""
 
     def wrapper(
@@ -70,9 +68,7 @@ class ActivitySensor:
 
     @staticmethod
     def is_below(entity: str, threshold: float) -> ActivitySensor:
-        return ActivitySensor(
-            entity, lambda s: float(s) < threshold, descriptor=f"< {threshold}"
-        )
+        return ActivitySensor(entity, lambda s: float(s) < threshold, descriptor=f"< {threshold}")
 
 
 @dataclasses.dataclass
@@ -94,6 +90,13 @@ class Room:
     # The last time there was motion in the room. Homeassistant "are lights on"
     # queries can have a bit of lag due to e.g. light transitions.
     last_motion: datetime.datetime
+    # Whether this model thinks the lights are on: if we tell HA to turn off
+    # the lights, it'll take maybe 5s before getting the light's state will
+    # return "off", because of transition + lag.
+    # This causes an edge case where movement while the light is turning off
+    # off prevents the lights from coming back on, until movement stops being
+    # detected and is subsequently detected again.
+    lights_on: bool
 
     @staticmethod
     def make_room(
@@ -104,7 +107,7 @@ class Room:
         lights_only_if: Optional[list[ActivitySensor]] = None,
         has_manual_control_toggle: bool = False,
     ) -> Room:
-        return Room(
+        room = Room(
             hass=hass,
             name=name,
             motion_sensor=f"binary_sensor.{name}_motion_occupancy",
@@ -112,17 +115,17 @@ class Room:
             activity_sensors=activity_sensors or [],
             lights_only_if=lights_only_if or [],
             manual_control_input_boolean=(
-                f"input_boolean.{name}_manual_control"
-                if has_manual_control_toggle
-                else None
+                f"input_boolean.{name}_manual_control" if has_manual_control_toggle else None
             ),
             last_motion=datetime.datetime.min,
+            lights_on=False
         )
+        # Initial seed for when developing.
+        room.lights_on = room._is_light_group_on()
+        return room
 
     def initialise(self, hass: hass.Hass):
-        hass.listen_state(
-            callback=self.on_room_motion, entity_id=self.motion_sensor, new="on"
-        )
+        hass.listen_state(callback=self.on_room_motion, entity_id=self.motion_sensor, new="on")
         hass.listen_state(
             callback=self.on_room_no_motion,
             entity_id=self.motion_sensor,
@@ -139,21 +142,17 @@ class Room:
             )
 
     def _turn_off_lights(self):
-        self.hass.call_service(
-            service="light/turn_off", transition=5, area_id=self.name
-        )
+        self.lights_on = False
+        self.hass.call_service(service="light/turn_off", transition=5, area_id=self.name)
+
+    def _is_light_group_on(self):
+        return get_state(self.hass, f"group.{self.name}_lights") == "off"
 
     def _get_active_sensors(self) -> list[ActivitySensor]:
-        return [
-            activity_sensor
-            for activity_sensor in self.activity_sensors
-            if activity_sensor.is_active(self.hass)
-        ]
+        return [activity_sensor for activity_sensor in self.activity_sensors if activity_sensor.is_active(self.hass)]
 
     def _get_inactive_required_sensors(self) -> list[ActivitySensor]:
-        return [
-            sensor for sensor in self.lights_only_if if not sensor.is_active(self.hass)
-        ]
+        return [sensor for sensor in self.lights_only_if if not sensor.is_active(self.hass)]
 
     def manual_control_enabled(self) -> bool:
         if self.manual_control_input_boolean is None:
@@ -173,31 +172,29 @@ class Room:
 
         inactive_required_sensors = self._get_inactive_required_sensors()
         if inactive_required_sensors:
-            self._verbose_log(
-                f"motion in {self.name} but required sensors aren't active: {inactive_required_sensors}"
-            )
+            self._verbose_log(f"motion in {self.name} but required sensors aren't active: {inactive_required_sensors}")
             return
-        area_lights_off = get_state(self.hass, f"group.{self.name}_lights") == "off"
+        area_lights_off = self._is_light_group_on()
         # Only load the scene if the lights are off: if the lights are already
         # on, leave them as they are.
-        if area_lights_off:
+        if area_lights_off or not self.lights_on:
             self._log(f"motion in {self.name}, loading default scene")
+            if not self.lights_on:
+                self._verbose_log("edge case: lights were on in HA but off in the model.")
             self.hass.fire_event(event="default_scene_turn_on", rooms=[self.name])
+            self.lights_on = True
         else:
             self._verbose_log(f"motion in {self.name} but lights already on")
 
     @callback
     def on_room_no_motion(self, *_):
         active_devices = self._get_active_sensors()
-        if not active_devices:
-            self._log(
-                f"no motion in {self.name} for {self.no_motion_timeout}, no devices are active, lights off"
-            )
-            self._turn_off_lights()
-        else:
-            self._verbose_log(
-                f"no motion in {self.name}, but devices are active: {active_devices}"
-            )
+        if active_devices:
+            self._verbose_log(f"no motion in {self.name}, but devices are active: {active_devices}")
+            return
+
+        self._log(f"no motion in {self.name} for {self.no_motion_timeout}, no devices are active, lights off")
+        self._turn_off_lights()
 
     @callback
     def on_activity_sensor_change(self, entity: str, *_):
@@ -259,9 +256,7 @@ class Lights(hass.Hass):
                 hass=self,
                 name="corridor",
                 no_motion_timeout=datetime.timedelta(minutes=2, seconds=30),
-                lights_only_if=[
-                    ActivitySensor.is_below("sensor.corridor_motion_illuminance", 35)
-                ],
+                lights_only_if=[ActivitySensor.is_below("sensor.corridor_motion_illuminance", 35)],
             ),
             Room.make_room(
                 hass=self,
