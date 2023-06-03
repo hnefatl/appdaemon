d0d581364babddb5647a4bddd51ad48febb9a876
Keith <hnefatl@gmail.com>
2023-06-03 17:59:47 +0200

    Actually apply typechecking, use `EntityId` consistently throughout.

--- apps/lights.py
+++ apps/lights.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import dataclasses
 import datetime
 
-from typing import Optional, Callable, Any, NewType
+from typing import Optional, Callable, Any, NewType, Coroutine
 
 import appdaemon.plugins.hass.hassapi as hass  # type: ignore
 
@@ -13,6 +13,13 @@ _VERBOSE_LOG = True
 
 
 EntityId = NewType("EntityId", str)
+
+
+# The appdaemon API doesn't have great typechecking since it allows for calling functions either sync or async: sync
+# invocations are inferred to have async returns, which causes type errors.
+# This hacky identity function tricks the typechecker into thinking the return is sync.
+def cast_sync(f: Coroutine) -> Any:
+    return f
 
 
 def callback(
@@ -37,7 +44,7 @@ def callback(
 
 
 def get_state(hass: hass.Hass, entity_id: EntityId) -> str:
-    state = hass.get_state(entity_id=str(entity_id))
+    state = cast_sync(hass.get_state(entity_id=str(entity_id)))
     if state is None:
         raise ValueError(f"Unknown entity {entity_id}")
     return state
@@ -72,7 +79,9 @@ class ActivitySensor:
 
     @staticmethod
     def is_below(entity: EntityId, threshold: float) -> ActivitySensor:
-        return ActivitySensor(entity, lambda s: float(s) < threshold, descriptor=f"< {threshold}")
+        return ActivitySensor(
+            entity, lambda s: float(s) < threshold, descriptor=f"< {threshold}"
+        )
 
 
 @dataclasses.dataclass
@@ -90,7 +99,7 @@ class Room:
     # Don't turn on the lights if any of these entities are on.
     lights_only_if: list[ActivitySensor]
     # input_boolean for toggling automatic/manual control.
-    manual_control_input_boolean: Optional[str]
+    manual_control_input_boolean: Optional[EntityId]
     # The last time there was motion in the room. Homeassistant "are lights on"
     # queries can have a bit of lag due to e.g. light transitions.
     last_motions: dict[EntityId, datetime.datetime]
@@ -113,7 +122,7 @@ class Room:
         has_manual_control_toggle: bool = False,
     ) -> Room:
         if motion_sensors is None:
-            motion_sensors = {f"binary_sensor.{name}_motion_occupancy"}
+            motion_sensors = {EntityId(f"binary_sensor.{name}_motion_occupancy")}
 
         room = Room(
             hass=hass,
@@ -122,9 +131,11 @@ class Room:
             no_motion_timeout=no_motion_timeout,
             activity_sensors=activity_sensors or [],
             lights_only_if=lights_only_if or [],
-            manual_control_input_boolean=(
-                f"input_boolean.{name}_manual_control" if has_manual_control_toggle else None
-            ),
+            manual_control_input_boolean=EntityId(
+                f"input_boolean.{name}_manual_control"
+            )
+            if has_manual_control_toggle
+            else None,
             last_motions={sensor: datetime.datetime.min for sensor in motion_sensors},
             lights_on=False,
         )
@@ -134,34 +145,52 @@ class Room:
 
     def initialise(self, hass: hass.Hass):
         for motion_sensor in self.motion_sensors:
-            hass.listen_state(callback=self.on_room_motion, entity_id=motion_sensor, new="on")
-            hass.listen_state(
-                callback=self.on_room_no_motion,
-                entity_id=motion_sensor,
-                new="off",
-                duration=self.no_motion_timeout.seconds,
+            cast_sync(
+                hass.listen_state(
+                    callback=self.on_room_motion, entity_id=motion_sensor, new="on"
+                )
+            )
+            cast_sync(
+                hass.listen_state(
+                    callback=self.on_room_no_motion,
+                    entity_id=motion_sensor,
+                    new="off",
+                    duration=self.no_motion_timeout.seconds,
+                )
             )
         # In addition to listening to just motion, also listen for when the
         # devices indicating activity in the room change, otherwise we might
         # leave lights on when a device changes state once we've left the room.
         for activity_sensor in self.activity_sensors:
-            hass.listen_state(
-                callback=self.on_activity_sensor_change,
-                entity_id=activity_sensor.entity,
+            cast_sync(
+                hass.listen_state(
+                    callback=self.on_activity_sensor_change,
+                    entity_id=activity_sensor.entity,
+                )
             )
 
-    def _turn_off_lights(self):
+    def _turn_off_lights(self) -> None:
         self.lights_on = False
-        self.hass.call_service(service="light/turn_off", transition=5, area_id=self.name)
+        cast_sync(
+            self.hass.call_service(
+                service="light/turn_off", transition=5, area_id=self.name
+            )
+        )
 
-    def _is_light_group_on(self):
-        return get_state(self.hass, f"group.{self.name}_lights") == "off"
+    def _is_light_group_on(self) -> bool:
+        return get_state(self.hass, EntityId(f"group.{self.name}_lights")) == "off"
 
     def _get_active_sensors(self) -> list[ActivitySensor]:
-        return [activity_sensor for activity_sensor in self.activity_sensors if activity_sensor.is_active(self.hass)]
+        return [
+            activity_sensor
+            for activity_sensor in self.activity_sensors
+            if activity_sensor.is_active(self.hass)
+        ]
 
     def _get_inactive_required_sensors(self) -> list[ActivitySensor]:
-        return [sensor for sensor in self.lights_only_if if not sensor.is_active(self.hass)]
+        return [
+            sensor for sensor in self.lights_only_if if not sensor.is_active(self.hass)
+        ]
 
     def manual_control_enabled(self) -> bool:
         if self.manual_control_input_boolean is None:
@@ -177,7 +206,7 @@ class Room:
 
     @callback
     def on_room_motion(self, entity_id: str, *_):
-        self.last_motions[entity_id] = datetime.datetime.now()
+        self.last_motions[EntityId(entity_id)] = datetime.datetime.now()
 
         inactive_required_sensors = self._get_inactive_required_sensors()
         if inactive_required_sensors:
@@ -191,20 +220,28 @@ class Room:
         if area_lights_off or not self.lights_on:
             self._log(f"motion in {self.name} from {entity_id}, loading default scene")
             if not self.lights_on:
-                self._verbose_log("edge case: lights were on in HA but off in the model.")
-            self.hass.fire_event(event="default_scene_turn_on", rooms=[self.name])
+                self._verbose_log(
+                    "edge case: lights were on in HA but off in the model."
+                )
+            cast_sync(self.hass.fire_event(event="default_scene_turn_on", rooms=[self.name]))
             self.lights_on = True
         else:
-            self._verbose_log(f"motion in {self.name} from {entity_id} but lights already on")
+            self._verbose_log(
+                f"motion in {self.name} from {entity_id} but lights already on"
+            )
 
     @callback
     def on_room_no_motion(self, *_):
         active_devices = self._get_active_sensors()
         if active_devices:
-            self._verbose_log(f"no motion in {self.name}, but devices are active: {active_devices}")
+            self._verbose_log(
+                f"no motion in {self.name}, but devices are active: {active_devices}"
+            )
             return
 
-        self._log(f"no motion in {self.name} for {self.no_motion_timeout}, no devices are active, lights off")
+        self._log(
+            f"no motion in {self.name} for {self.no_motion_timeout}, no devices are active, lights off"
+        )
         self._turn_off_lights()
 
     @callback
@@ -217,7 +254,9 @@ class Room:
         # Don't turn the lights off if a device is turned off and there hasn't
         # been motion recently: wait for the full timeout.
         now = datetime.datetime.now()
-        time_since_last_motion = min(now - last_motion for last_motion in self.last_motions.values())
+        time_since_last_motion = min(
+            now - last_motion for last_motion in self.last_motions.values()
+        )
         if time_since_last_motion > self.no_motion_timeout:
             self._log(
                 f"no active devices, last was {entity}, last motion was "
@@ -240,39 +279,41 @@ class Lights(hass.Hass):
                 hass=self,
                 name="office",
                 no_motion_timeout=datetime.timedelta(hours=1),
-                activity_sensors=[ActivitySensor.is_on("switch.pc")],
+                activity_sensors=[ActivitySensor.is_on(EntityId("switch.pc"))],
             ),
             Room.make_room(
                 hass=self,
                 name="living_room",
                 no_motion_timeout=datetime.timedelta(minutes=15),
                 motion_sensors={
-                    "binary_sensor.living_room_motion_occupancy",
-                    "binary_sensor.reading_chair_motion_occupancy",
+                    EntityId("binary_sensor.living_room_motion_occupancy"),
+                    EntityId("binary_sensor.reading_chair_motion_occupancy"),
                 },
                 activity_sensors=[
-                    ActivitySensor.isnt_off("media_player.shield"),
-                    ActivitySensor.isnt_off("media_player.living_room_speaker"),
+                    ActivitySensor.isnt_off(EntityId("media_player.shield")),
+                    ActivitySensor.isnt_off(EntityId("media_player.living_room_speaker")),
                 ],
             ),
             Room.make_room(
                 hass=self,
                 name="bathroom",
                 no_motion_timeout=datetime.timedelta(minutes=2),
-                activity_sensors=[ActivitySensor.is_on("input_boolean.shower_active")],
+                activity_sensors=[ActivitySensor.is_on(EntityId("input_boolean.shower_active"))],
             ),
             Room.make_room(
                 hass=self,
                 name="bedroom",
                 no_motion_timeout=datetime.timedelta(minutes=1),
-                lights_only_if=[ActivitySensor.is_on("input_boolean.keith_awake")],
+                lights_only_if=[ActivitySensor.is_on(EntityId("input_boolean.keith_awake"))],
                 has_manual_control_toggle=True,
             ),
             Room.make_room(
                 hass=self,
                 name="corridor",
                 no_motion_timeout=datetime.timedelta(minutes=2, seconds=30),
-                lights_only_if=[ActivitySensor.is_below("sensor.corridor_motion_illuminance", 35)],
+                lights_only_if=[
+                    ActivitySensor.is_below(EntityId("sensor.corridor_motion_illuminance"), 35)
+                ],
                 has_manual_control_toggle=True,
             ),
             Room.make_room(
