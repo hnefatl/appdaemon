f9fc79bad575f41f01b681db4e3e5d347b64dcaa
Keith Collister <hnefatl@gmail.com>
2023-10-17 20:22:48 +0200

    Support group/light APIs.

--- apps/caramelldansen.py
+++ apps/caramelldansen.py
@@ -7,10 +7,12 @@ import asyncio
 import itertools
 import datetime
 
-from phue import Bridge, PhueRegistrationException
+from typing import cast, Any
+from phue import Bridge, Group, PhueRegistrationException
 
 
-ROOM_SELECTOR = "input_select.caramell_dansen_room"
+ROOM_SELECTOR_ENTITY_ID = "input_select.caramell_dansen_room"
+USE_GROUP_API_ENTITY_ID = "input_boolean.caramell_dansen_use_group_api"
 SPEED = 0.2
 RED = 2000
 CYAN = 39000
@@ -30,9 +32,11 @@ class CaramellDansen(hass.Hass):
         print(f"All Hue groups: {[g.name for g in self.connect().groups]}")
 
         # Turn off any active effects, reset input.
-        self.set_state(entity_id=ROOM_SELECTOR, state="")
+        self.set_state(entity_id=ROOM_SELECTOR_ENTITY_ID, state="")
 
-        self.listen_state(entity_id=ROOM_SELECTOR, callback=self.room_selection_changed)
+        self.listen_state(
+            entity_id=ROOM_SELECTOR_ENTITY_ID, callback=self.room_selection_changed
+        )
 
     def connect(self):
         try:
@@ -44,17 +48,31 @@ class CaramellDansen(hass.Hass):
             )
 
     async def room_selection_changed(self, _entity, _attribute, _old, new, _kwargs):
-        group_id = new
-        if not group_id:
+        group_name = new
+        if not group_name:
             # Room deselected, stop the effect.
             self._effect_active = False
             return
 
+        use_group_api: bool = self.get_state(entity_id=USE_GROUP_API_ENTITY_ID) == "on"
+
         bridge = self.connect()
+
+        if use_group_api:
+            update_lights = lambda params: bridge.set_group(
+                group_name, params, transitiontime=0
+            )
+        else:
+            group = Group(bridge, group_name)
+            light_ids = [light.light_id for light in group.lights]
+
+            update_lights = lambda params: bridge.set_light(
+                light_ids, params, transitiontime=0
+            )
 
         # Initialise the lights
         initial_params = {"on": True, "bri": 255}
-        bridge.set_group(group_id, initial_params, transitiontime=0)
+        update_lights(initial_params)
 
         self._effect_active = True
         # Run the loop until indicated not to
@@ -62,6 +80,5 @@ class CaramellDansen(hass.Hass):
             if not self._effect_active:
                 break
 
-            # Set 0 transition time for a snappy flick between colours.
-            bridge.set_group(group_id, "hue", hue, transitiontime=0)
+            update_lights({"hue": hue})
             await asyncio.sleep(SPEED)
