1dd82befddac4d71e84a44c28e24da95ae3bdd48
Keith <hnefatl@gmail.com>
2024-08-25 22:21:31 +0100

    Add new bathroom light switch.

--- apps/zha_buttons.py
+++ apps/zha_buttons.py
@@ -104,6 +104,7 @@ class IkeaDimmer(Button):
 DEVICE_MAPPING: Dict[str, Button] = {
     "08a5b2fcc6bab34e04c26f24b04ba75f": IkeaDimmer("espresso_machine_switch"),
     "406a8b92e13d77d79941d59e37f03211": IkeaRemote("bedroom_remote"),
+    "132631a4a3ccafe42b642066622f70ca": IkeaDimmer("bathroom_switch"),
 }
 IGNORED_DEVICES = {
     "ea07540a8e0dab2abaab5c804466465a",
