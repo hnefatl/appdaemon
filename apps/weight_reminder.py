d0bef5e7934894c64ade00afac9ddc26c24d0ab0
Keith <hnefatl@gmail.com>
2024-09-26 20:27:06 +0100

    Less `datetime.` in weight reminder script.

--- apps/weight_reminder.py
+++ apps/weight_reminder.py
@@ -1,8 +1,7 @@
 from typing import Any
 
-import datetime
-import dateutil
-import dateutil.tz
+from datetime import datetime, timedelta
+from dateutil.tz import tzlocal
 
 import typed_hass
 from typed_hass import EntityId
@@ -13,30 +12,28 @@ BEDROOM_SENSORS = [
 ]
 SHOWER_ACTIVE = EntityId("input_boolean.shower_active")
 
-LOCAL_TZ = dateutil.tz.tzlocal()
 
 class WeightReminder(typed_hass.Hass):
     def initialize(self):
         # Last activation. Default to as old as possible.
-        self._last_shower_on = datetime.datetime.fromtimestamp(0, LOCAL_TZ)
-        self._last_reminder = datetime.datetime.fromtimestamp(0, LOCAL_TZ)
+        self._last_shower_on = datetime.fromtimestamp(0, tzlocal())
+        self._last_reminder = datetime.fromtimestamp(0, tzlocal())
 
         for bedroom_sensor in BEDROOM_SENSORS:
             self.listen_state(callback=self.bedroom_motion, entity_id=bedroom_sensor)
         self.listen_state(callback=self.shower_on, entity_id=SHOWER_ACTIVE)
 
     def bedroom_motion(self, *_: Any):
-        now = datetime.datetime.now(tz=LOCAL_TZ)
+        now = datetime.now(tz=tzlocal())
 
-        recent_after_shower = now < self._last_shower_on + datetime.timedelta(
-            minutes=30
-        )
+        recent_after_shower = now < self._last_shower_on + timedelta(minutes=30)
         havent_reminded_today = self._last_reminder.date() < now.date()
         if recent_after_shower and havent_reminded_today:
             self._last_reminder = now
             self.tts_speak(
-                message="Weigh yourself.", media_player=EntityId("media_player.bedroom_speaker")
+                message="Weigh yourself.",
+                media_player=EntityId("media_player.bedroom_speaker"),
             )
 
     def shower_on(self, *_: Any):
-        self._last_shower_on = datetime.datetime.now()
+        self._last_shower_on = datetime.now()
