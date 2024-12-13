e36a70fe5004c3785a22eb60c0e70dcf714f40b2
Keith <hnefatl@gmail.com>
2024-12-13 23:29:38 +0000

    Make room-specific manual control, with persistent brightness between colours

--- apps/remotes.py
+++ apps/remotes.py
@@ -2,7 +2,6 @@
 
 # pyright: reportUnknownMemberType=false, reportUnknownVariableType=false
 
-import copy
 import dataclasses
 from typing import Any, Callable, Dict
 
@@ -11,57 +10,104 @@ import appdaemon.plugins.hass.hassapi as hass  # pyright: ignore[reportMissingTy
 import zha_buttons
 import default_scene_service
 
+
 @dataclasses.dataclass
-class Scene:
-    entity_id: str
-    color_name: str | None = None
-    brightness_pct: float | None = None
+class SceneState:
+    colour: str
+    brightness: float
 
-    def to_turn_on_args(self) -> dict[str, Any]:
-        return dataclasses.asdict(self)
 
-# "scenes", but really just arguments to turn_on
-_MANUAL_MODE_SCENES = (
-    Scene(
-        entity_id="group.bedroom_lights",
-        color_name="orange",
-        brightness_pct=40,
-    ),
-    Scene(
-        entity_id="group.bedroom_lights",
-        color_name="red",
-        brightness_pct=80,
-    ),
-    Scene(
-        entity_id="group.bedroom_lights",
-        color_name="green",
-        brightness_pct=80,
-    ),
-    Scene(
-        entity_id="group.bedroom_lights",
-        color_name="blue",
-        brightness_pct=80,
-    ),
-    Scene(
-        entity_id="group.bedroom_lights",
-        color_name="purple",
-        brightness_pct=80,
-    ),
-    Scene(entity_id="scene.bedroom_arctic_aurora"),
-)
+class RoomController:
+    def __init__(
+        self,
+        hass: hass.Hass,
+        room_name: str,
+        lights_entity_id: str,
+        manual_control_entity_id: str,
+        colours: list[str],
+    ):
+        self._hass = hass
+        # Immutable
+        self._room_name = room_name
+        self._lights_entity_id = lights_entity_id
+        self._manual_control_entity_id = manual_control_entity_id
+        self._colours = colours
+        self._default_brightness = 80.0
+
+        # Mutable
+        self._scene: SceneState | None = None
+        self._colour_iterator = 0
+
+    def handle(
+        self,
+        _device: zha_buttons.Button,
+        button: str,
+        press: zha_buttons.ButtonPress,
+    ):
+        manual_control = self._hass.get_entity(self._manual_control_entity_id)
+        is_manual_control = manual_control.get_state() == "on"
+
+        if button == "centre" and press is zha_buttons.ButtonPress.SINGLE:
+            manual_control.toggle()
+            is_manual_control = not is_manual_control
+
+            self._hass.log(f"manual lights set to: {is_manual_control}")
+            if not is_manual_control:
+                self._scene = None
+                self._hass.fire_event(
+                    event=default_scene_service.EVENT_NAME,
+                    rooms=[self._room_name],
+                )
+                return
+
+        if not is_manual_control:
+            self._hass.log("manual control disabled, ignoring")
+            return
+        if self._scene is None:
+            self._colour_iterator = 0
+            self._scene = SceneState(
+                colour=self._colours[self._colour_iterator],
+                brightness=self._default_brightness,
+            )
+
+        if button in {"left", "right"}:
+            if button == "right":
+                self._colour_iterator += 1
+            elif button == "left":
+                self._colour_iterator -= 1
+
+            self._colour_iterator %= len(self._colours)
+            self._scene.colour = self._colours[self._colour_iterator]
+
+        if button in {"top", "bottom"}:
+            if button == "top":
+                self._scene.brightness = min(self._scene.brightness + 20, 100)
+            elif button == "bottom":
+                self._scene.brightness = max(self._scene.brightness - 20, 0)
+
+        self._hass.log(f"manual scene to: {self._scene}")
+        self._hass.turn_on(
+            entity_id=self._lights_entity_id,
+            color_name=self._scene.colour,
+            brightness_pct=self._scene.brightness,
+        )
 
 
 class Remotes(hass.Hass):
 
     def __init__(self, *args: ..., **kwargs: ...):
         super().__init__(*args, **kwargs)
-        self._manual_mode_scene_iterator = 0
-        self._scene: Scene | None = None
 
-        self._behaviours: dict[
+        self._controllers: dict[
             str, Callable[[zha_buttons.Button, str, zha_buttons.ButtonPress], None]
         ] = {
-            "bedroom_remote": self._bedroom_remote_logic,
+            "bedroom_remote": RoomController(
+                self,
+                room_name="bedroom",
+                lights_entity_id="group.bedroom_lights",
+                manual_control_entity_id="input_boolean.bedroom_manual_control",
+                colours=["orange", "red", "green", "blue", "purple"],
+            )
         }
 
     def initialize(self):
@@ -74,52 +120,5 @@ class Remotes(hass.Hass):
             return
         (device, _button, _press) = info
 
-        if behaviour := self._behaviours.get(device.name):
-            behaviour(*info)
-
-    def _bedroom_remote_logic(
-        self,
-        device: zha_buttons.Button,
-        button: str,
-        press: zha_buttons.ButtonPress,
-    ):
-        bedroom_manual_control = self.get_entity("input_boolean.bedroom_manual_control")
-        is_manual_control = bedroom_manual_control.get_state() == "on"
-
-        if button == "centre" and press is zha_buttons.ButtonPress.SINGLE:
-            bedroom_manual_control.toggle()
-            is_manual_control = not is_manual_control
-
-            self.log(f"manual lights set to: {is_manual_control}")
-            if not is_manual_control:
-                self._scene = None
-                self.fire_event(
-                    event=default_scene_service.EVENT_NAME,
-                    rooms=["bedroom"],
-                )
-                return
-
-        if not is_manual_control:
-            self.log("manual control disabled, ignoring")
-            return
-        if self._scene is None:
-            self._manual_mode_scene_iterator = 0
-            self._scene = copy.deepcopy(_MANUAL_MODE_SCENES[self._manual_mode_scene_iterator])
-
-        if button in {"left", "right"}:
-            if button == "right":
-                self._manual_mode_scene_iterator += 1
-            elif button == "left":
-                self._manual_mode_scene_iterator -= 1
-
-            self._manual_mode_scene_iterator %= len(_MANUAL_MODE_SCENES)
-            self._scene = copy.deepcopy(_MANUAL_MODE_SCENES[self._manual_mode_scene_iterator])
-
-        if button in {"top", "bottom"}:
-            if button == "top":
-                self._scene.brightness_pct = min(self._scene.brightness_pct + 20, 100)
-            elif button == "bottom":
-                self._scene.brightness_pct = max(self._scene.brightness_pct - 20, 0)
-
-        self.log(f"manual scene to: {self._scene}")
-        self.turn_on(**self._scene.to_turn_on_args())
+        if controller := self._controllers.get(device.name):
+            controller.handle(*info)
